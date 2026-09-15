4344400fe0da9b2e21e445aa6b82436b872f1ec9
xani13 <mkgsani9@gmail.com>
2026-09-15 22:39:16 +0530

    Deploy static site directly on Vercel: Rename root server.js to dev-server.js and configure clean api rewrites

--- api/index.js
+++ api/index.js
@@ -1,4 +1,4 @@
-import { handleRequest } from '../server.js';
+import { handleRequest } from '../dev-server.js';
 
 export default async function handler(req, res) {
   return handleRequest(req, res);
